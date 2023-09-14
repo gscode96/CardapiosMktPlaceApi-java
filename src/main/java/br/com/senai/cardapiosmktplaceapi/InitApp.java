619ebc0f97aaac1dc0d7203347c5330738857b62
gs.code96 <gs.code96@gmail.com>
2023-09-14 20:30:04 -0300

    Iniciado controller

--- src/main/java/br/com/senai/cardapiosmktplaceapi/InitApp.java
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/InitApp.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
+
 import br.com.senai.cardapiosmktplaceapi.repository.CategoriasRepository;
 import br.com.senai.cardapiosmktplaceapi.repository.RestaurantesRepository;
 
@@ -21,6 +23,12 @@ public class InitApp {
 	public static void main(String[] args) {
 		SpringApplication.run(InitApp.class, args);
 
+	}
+
+	@Bean
+	public Hibernate5JakartaModule jsonHibernate5Module() {
+
+		return new Hibernate5JakartaModule();
 	}
 
 	@Bean
