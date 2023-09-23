fbbab6b90dc4206cc8b36eb0e994892c436c3c13
gs.code96 <gs.code96@gmail.com>
2023-09-22 22:14:52 -0300

    Implementado seguranca da api

---
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/repository/UsuariosRepository.java
@@ -0,0 +1,13 @@
+package br.com.senai.cardapiosmktplaceapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.com.senai.cardapiosmktplaceapi.entity.Usuario;
+
+public interface UsuariosRepository extends JpaRepository<Usuario, String>{
+	
+	@Query(value = "SELECT u FROM Usuario u WHERE u.login = :login")
+	public Usuario buscarPor(String login);
+	
+}
